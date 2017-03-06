11a3cab343fcda7b2195b47045fd74df1402668f
Pranesh Kumar <praneshkmr@gmail.com>
2017-03-07 00:17:35 +0530

    Logic for transformation of Clustering result

--- app/services/LocationService.js
+++ app/services/LocationService.js
@@ -76,7 +76,39 @@ LocationService.prototype.setRandomLocations = function (callback) {
 }
 
 LocationService.prototype.buildBoundary = function (callback) {
-    locationSearch.buildBoundary(callback);
+    locationSearch.buildBoundary(function (err, result) {
+        if (err) {
+            callback(err);
+        }
+        else {
+            transformBoundaryResult(result, callback);
+        }
+    });
+}
+
+function transformBoundaryResult(result, callback) {
+    var buckets = result.aggregations.bangaloreClusters.buckets;
+    var clusters = []
+    buckets.forEach(function (bucket) {
+        var cluster = {
+            geoHash: bucket.key,
+            totalCount: bucket.doc_count,
+            count: {
+                drivers: null,
+                customers: null
+            }
+        }
+        bucket.types.buckets.forEach(function (type) {
+            if (type.key === "customer") {
+                cluster.count.customers = type.doc_count;
+            }
+            else if (type.key === "driver") {
+                cluster.count.drivers = type.doc_count;
+            }
+        }, this);
+        clusters.push(cluster);
+    }, this);
+    callback(null, clusters);
 }
 
 module.exports = LocationService;
