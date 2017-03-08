8280a46b7333bd431875d4396d6c2d6abe5092f0
Pranesh Kumar <praneshkmr@gmail.com>
2017-03-08 15:23:24 +0530

    Basic ws setup and bootstap page

--- app.js
+++ app.js
@@ -97,4 +97,25 @@ function onListening() {
   debug('Listening on ' + bind);
 }
 
+
+
+//WebSockets
+var WebSocket = require('ws');
+var wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+app.set('wss',wss);
+
+
 module.exports = app;
