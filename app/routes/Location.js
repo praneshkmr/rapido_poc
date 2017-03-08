7c8ffa8701a0e46d852536216d4c18b3e9397d0c
Pranesh Kumar <praneshkmr@gmail.com>
2017-03-08 17:04:50 +0530

    Logic to plot the location on map

---
+++ app/routes/Location.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var LocationService = require('./../services/LocationService');
+var locationService = new LocationService();
+
+router.get('/all', function (req, res, next) {
+    locationService.getAllLocations(function(err, result){
+        if(err){
+            console.error(err);
+            res.status(500).send(err);
+        }
+        else{
+            res.send(result);
+        }
+    })
+});
+
+module.exports = router;
