be432150f9a962b80a235ee4299ef7d146ea2eef
Pranesh Kumar <praneshkmr@gmail.com>
2017-03-08 11:03:31 +0530

    Copied required content from www to app.js for WS

--- app.js
+++ app.js
@@ -43,4 +43,58 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//Starting the Web Server
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+app.on('error', onError);
+app.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
 module.exports = app;
