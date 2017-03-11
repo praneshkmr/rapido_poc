b440395db3f3da332b79465da5e943d3209ba16b
Pranesh Kumar <praneshkmr@gmail.com>
2017-03-11 14:20:34 +0530

    Logic to Send and Receive Location with web socket

--- app.js
+++ app.js
@@ -109,6 +109,7 @@ var wss = new WebSocket.Server({ server });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    wss.broadcast(message);
   });
 });
 
